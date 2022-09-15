dc6387cbab8836e9e59bbe8c9f60e70b55337e77
ChinweIjy1 <casuoha@gmail.com>
2022-09-15 13:48:31 -0700

    A function that prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,53 +1,46 @@
-#include <math.h>
 #include "main.h"
+
 /**
- * power - exponents
- * @base: base
- * @exp: exponent
- * Return: result (int)
-*/
-int  power(int base, int exp)
-{
-int i, num;
-num = 1;
-for (i = 0; i < exp; ++i)
-num *= base;
-return (num);
-}
-/**
- * print_number - prints an integer
- * @n: number to print
- * Return void
+ * print_number - print an int numbers
+ * @n: number tested
+ * Return: Always 0
 */
 void print_number(int n)
 {
-int negative = 0;
-int digit;
-int divisor;
-int begin = 0;
-int place = 10;
+int i, j, digit, digits, power;
+unsigned int temp, numchar, number;
+
+digit = 0;
 if (n < 0)
 {
-negative = 1;
-n = n * -1;
-}
-while (place >= 0)
-{
-/*divisor = pow(10, place);*/
-divisor = power(10, place);
-digit = ((n / divisor) % 10);
-if (digit == 0 && begin == 0)
-{
-place--;
+_putchar('-');
+temp = -n;
 }
 else
 {
-_putchar('0' + digit);
-place--;
+temp = n;
 }
-}
-if (digit == 0 && divisor == 1)
+number = temp;
+
+while (number >= 10)
 {
-_putchar(48);
+number = number / 10;
+digit++;
+}
+digits = digit + 1;
+power = 1;
+i = 1;
+
+while (i < digits)
+{
+power = power * 10;
+i++
+}
+j = power;
+while (j >= 1)
+{
+numchar = (temp / j) % 10;
+_putchar(numchar + '0');
+j = j / 10;
 }
 }
